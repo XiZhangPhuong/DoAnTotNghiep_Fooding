0f51f8c5d2779e1608fef75a8d611c2c2fb6d19c
TRUONGQUYEN <kymonos1@gmail.com>
2023-02-16 11:12:59 +0700

    quyen: add libary

---
+++ lib/resources/auth_method.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fooding_project/resources/storage_method.dart';
+import 'package:instagram_flutter/Models/user.dart' as model;
+import 'package:instagram_flutter/resources/storage_method.dart';
+
+class AuthMethod {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<model.User> getUserDetails() async {
+    User user = _auth.currentUser!;
+
+    DocumentSnapshot snap =
+        await _firestore.collection("users").doc(user.uid).get();
+
+    return model.User.fromSnap(snap);
+  }
+
+  //sign in  user
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+  }) async {
+    String res = "some erorr";
+    try {
+      if (email.isNotEmpty &&
+          password.isNotEmpty &&
+          username.isNotEmpty &&
+          bio.isNotEmpty) {
+        final cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        String strPhotoURL = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
+        model.User user = model.User(
+          email: email,
+          uid: cred.user!.uid.toString(),
+          photoUrl: strPhotoURL,
+          username: username,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
+
+        _firestore.collection('users').doc(cred.user!.uid).set(user.toJson());
+        res = 'success';
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+  //logging in user
+
+  Future<String> loginUser(
+      {required String email, required String password}) async {
+    String res = "some error";
+    try {
+      if (email.isNotEmpty && password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        res = "success";
+      } else {
+        res = "please enter all fields";
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:fooding_project/resources/storage_method.dart';
+import 'package:instagram_flutter/Models/post.dart';
+import 'package:instagram_flutter/resources/storage_method.dart';
+import 'package:uuid/uuid.dart';
+
+class FireStoreMethod {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //upload post
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    String res = "some error";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+      String postId = const Uuid().v1();
+      Post post = Post(
+        description: description,
+        uid: uid,
+        postId: postId,
+        username: username,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+        likes: [],
+      );
+      _firestore.collection('posts').doc(postId).set(
+            post.toJson(),
+          );
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
