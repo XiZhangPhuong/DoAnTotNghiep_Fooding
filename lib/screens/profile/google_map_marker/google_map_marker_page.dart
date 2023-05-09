8aba7caf1d61de910c7f53d723b722a77b37a9b1
TRUONGQUYEN <kymonos1@gmail.com>
2023-05-10 00:55:11 +0700

    update something better

--- lib/screens/profile/google_map_marker/google_map_marker_page.dart
+++ lib/screens/profile/google_map_marker/google_map_marker_page.dart
@@ -5,6 +5,8 @@ import 'package:fooding_project/screens/profile/google_map_marker/google_map_mar
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import '../../../utils/color_resources.dart';
+
 class GoogleMapMarkerPage extends GetView {
   const GoogleMapMarkerPage({super.key});
 
@@ -22,20 +24,57 @@ class GoogleMapMarkerPage extends GetView {
                   ? const Center(
                       child: CircularProgressIndicator(),
                     )
-                  : GoogleMap(
-                      initialCameraPosition: CameraPosition(
-                        target: controller.markerCustomer!.position,
-                        zoom: 14.4746,
-                      ),
-                      markers: {
-                        controller.markerCustomer!,
-                        controller.markerStore!,
-                        controller.markerShiper!,
-                      },
-                      myLocationButtonEnabled: false,
-                      onMapCreated: (GoogleMapController googleMapController) {
-                        controller.ggMapcontroller = googleMapController;
-                      },
+                  : Stack(
+                      children: [
+                        Positioned(
+                          child: GestureDetector(
+                            onTap: () {
+                              Get.back();
+                            },
+                            child: Container(
+                              margin: EdgeInsets.only(
+                                left: IZIDimensions.SPACE_SIZE_2X,
+                                top: IZIDimensions.SPACE_SIZE_1X,
+                                right: IZIDimensions.SPACE_SIZE_1X,
+                                bottom: IZIDimensions.SPACE_SIZE_1X,
+                              ),
+                              height: IZIDimensions.ONE_UNIT_SIZE * 100,
+                              width: IZIDimensions.ONE_UNIT_SIZE * 50,
+                              decoration: const BoxDecoration(
+                                color: ColorResources.WHITE,
+                                shape: BoxShape.circle,
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                  left: IZIDimensions.SPACE_SIZE_1X * 1.5,
+                                ),
+                                child: const Icon(
+                                  Icons.arrow_back_ios,
+                                  color: ColorResources.colorTextTabbar,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned.fill(
+                          child: GoogleMap(
+                            initialCameraPosition: CameraPosition(
+                              target: controller.markerCustomer!.position,
+                              zoom: 14.4746,
+                            ),
+                            markers: {
+                              controller.markerCustomer!,
+                              controller.markerStore!,
+                              controller.markerShiper!,
+                            },
+                            myLocationButtonEnabled: false,
+                            onMapCreated:
+                                (GoogleMapController googleMapController) {
+                              controller.ggMapcontroller = googleMapController;
+                            },
+                          ),
+                        )
+                      ],
                     ),
             );
           },
